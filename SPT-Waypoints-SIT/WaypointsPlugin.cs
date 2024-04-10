b4f1aa5d2185d5c8a8ec7d9fce6e504d18360f44
Insave <froststang@gmail.com>
2024-04-10 09:17:35 -0500

    Update Waypoints to 1.4.1

--- SPT-Waypoints-SIT/WaypointsPlugin.cs
+++ SPT-Waypoints-SIT/WaypointsPlugin.cs
@@ -1,53 +1,51 @@
 ﻿using BepInEx;
+using DrakiaXYZ.Helpers;
 using DrakiaXYZ.Waypoints.Helpers;
 using DrakiaXYZ.Waypoints.Patches;
+using DrakiaXYZ.Waypoints.VersionChecker;
 using System;
 using System.IO;
 using System.Reflection;
 
 namespace DrakiaXYZ.Waypoints
 {
-    [BepInPlugin("xyz.drakia.waypoints", "DrakiaXYZ-Waypoints", "1.3.0")]
+    [BepInPlugin("xyz.drakia.waypoints", "DrakiaXYZ-Waypoints", "1.4.1")]
     public class WaypointsPlugin : BaseUnityPlugin
     {
         public static string PluginFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        public static string CustomFolder = Path.Combine(PluginFolder, "custom");
         public static string MeshFolder = Path.Combine(PluginFolder, "mesh");
         public static string PointsFolder = Path.Combine(PluginFolder, "points");
         public static string NavMeshFolder = Path.Combine(PluginFolder, "navmesh");
 
         private void Awake()
         {
-            Settings.Init(Config);
+            if (!TarkovVersion.CheckEftVersion(Logger, Info, Config))
+            {
+                throw new Exception($"Invalid EFT Version");
+            }
 
-            // Make sure plugin folders exist
-            Directory.CreateDirectory(CustomFolder);
-            Directory.CreateDirectory(NavMeshFolder);
-            Logger.LogInfo($"Remember to install navmashes to {NavMeshFolder} for Waypoints to work!");
-            //Logger.LogDebug(NavMeshFolder);
+            if (!DependencyChecker.ValidateDependencies(Logger, Info, this.GetType(), Config))
+            {
+                throw new Exception($"Missing Dependencies");
+            }
+
+            Settings.Init(Config);
 
             try
             {
-                CustomWaypointLoader.Instance.loadData();
-
                 new DebugPatch().Enable();
-                //new WaypointPatch().Enable();
-                //new BotOwnerRunPatch().Enable();
-
-                new EditorPatch().Enable();
-
-                new DoorBlockerPatch().Enable();
+                new WaypointPatch().Enable();
+                new DoorLinkPatch().Enable();
+                new DoorLinkStateChangePatch().Enable();
+                new SwitchDoorBlockerPatch().Enable();
+                new ExfilDoorBlockerPatch().Enable();
+                new FindPathPatch().Enable();
             }
             catch (Exception ex)
             {
                 Logger.LogError($"{GetType().Name}: {ex}");
                 throw;
             }
-
-            // Note: We only include this in debug builds for now, because we're not shipping BigBrain
-            //#if DEBUG
-            //BrainManager.AddCustomLayer(typeof(RoamingLayer), new List<string>() { "Assault", "PMC" }, 2);
-            //#endif
         }
     }
 }
